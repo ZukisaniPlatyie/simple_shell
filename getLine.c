81e0861c271aefc3b59d76657440ff049f06f587
ZukisaniPlatyie <platyieza@gmail.com>
2023-05-25 07:12:05 -0700

    0x16. C - Simple Shell

--- getLine.c
+++ getLine.c
@@ -34,7 +34,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 			info->linecount_flag = 1;
 			remove_comments(*buf);
 			build_history_list(info, *buf, info->histcount++);
-			 if (_strchr(*buf, ';')) /*is this a command chain? */
+			/* if (_strchr(*buf, ';')) is this a command chain? */
 			{
 				*len = r;
 				info->cmd_buf = buf;
